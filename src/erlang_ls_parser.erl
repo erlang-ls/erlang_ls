b95554838c271596fca2ba00de8b0570336c02cb
Roberto Aloi <robertoaloi@users.noreply.github.com>
2019-08-07 18:25:37 +0200

    [#37] Extract locations for export entries

--- src/erlang_ls_parser.erl
+++ src/erlang_ls_parser.erl
@@ -2,6 +2,7 @@
 
 -export([ parse/1
         , parse_file/1
+        , parse_exports/1
         ]).
 
 %% TODO: Generate random filename
@@ -29,6 +30,46 @@ parse_file(Path) ->
       Tree = erl_syntax:form_list(Forms),
       ok = file:close(IoDevice),
       {ok, Tree};
+    {error, Error} ->
+      {error, Error}
+  end.
+
+-spec parse_exports(binary()) ->
+   {ok, [[erlang_ls_poi:pos()]]} | {error, any()}.
+parse_exports(Path) ->
+  case file:open(Path, [read]) of
+    {ok, IoDevice} ->
+      Res = parse_exports(IoDevice, [], {1, 1}),
+      ok = file:close(IoDevice),
+      {ok, Res};
+    {error, Error} ->
+      {error, Error}
+  end.
+
+-spec parse_exports(io:device(), [any()], erlang_ls_poi:pos()) ->
+   {ok, [[erlang_ls_poi:pos()]]} | {error, any()}.
+parse_exports(IoDevice, Acc, StartLocation) ->
+  case io:scan_erl_form(IoDevice, "", StartLocation) of
+    {ok, Tokens, EndLocation} ->
+      case erl_parse:parse_form(Tokens) of
+        {ok, Form} ->
+          case erl_syntax:type(Form) of
+            attribute ->
+              case erl_syntax_lib:analyze_attribute(Form) of
+                {export, Exports} ->
+                  Locations = [L || {atom, L, F} <- Tokens, F =/= export],
+                  parse_exports(IoDevice, [lists:zip(Exports, Locations)|Acc], EndLocation);
+                _ ->
+                  parse_exports(IoDevice, Acc, EndLocation)
+              end;
+            _ ->
+              parse_exports(IoDevice, Acc, EndLocation)
+          end;
+        {error, _Error} ->
+          parse_exports(IoDevice, Acc, EndLocation)
+      end;
+    {eof, _} ->
+      lists:reverse(Acc);
     {error, Error} ->
       {error, Error}
   end.
