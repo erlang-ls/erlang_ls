13787ee6b5972eb734b8d73b5a7d18c7b2596926
Roberto Aloi <robertoaloi@users.noreply.github.com>
2019-10-26 18:32:59 +0200

    [#35] Add getters for POI

--- src/erlang_ls_poi.erl
+++ src/erlang_ls_poi.erl
@@ -3,7 +3,14 @@
 %%==============================================================================
 -module(erlang_ls_poi).
 
+%% Constructor
 -export([ new/4 ]).
+
+%% Getters
+-export([ data/1
+        , kind/1
+        , range/1
+        ]).
 
 -export([ match_pos/2 ]).
 
@@ -19,21 +26,27 @@
 %% @edoc Constructor for a Point of Interest.
 -spec new(tree(), poi_kind(), any(), extra()) -> poi().
 new(Tree, Kind, Data, Extra) ->
-  Pos = erl_syntax:get_pos(Tree),
+  Pos   = erl_syntax:get_pos(Tree),
   Range = erlang_ls_range:range(Pos, Kind, Data, Extra),
   #{ kind  => Kind
    , data  => Data
    , range => Range
    }.
 
+-spec data(poi()) -> any().
+data(#{ data := Data }) ->
+  Data.
+
+-spec kind(poi()) -> any().
+kind(#{ kind := Kind }) ->
+  Kind.
+
+-spec range(poi()) -> any().
+range(#{ range := Range }) ->
+  Range.
+
 -spec match_pos([poi()], pos()) -> [poi()].
 match_pos(POIs, Pos) ->
-  [POI || #{range := Range} = POI <- POIs, matches_pos(Pos, Range)].
-
-%%==============================================================================
-%% Internal Functions
-%%==============================================================================
-
--spec matches_pos(pos(), poi_range()) -> boolean().
-matches_pos(Pos, #{from := From, to := To}) ->
-  (From =< Pos) andalso (Pos =< To).
+  [POI || #{range := #{ from := From
+                      , to    := To
+                      }} = POI <- POIs, (From =< Pos) andalso (Pos =< To)].
