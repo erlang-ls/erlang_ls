c97e5e1e95eb0fd421f888597a32a080326fbf27
Roberto Aloi <robertoaloi@users.noreply.github.com>
2019-10-26 20:50:50 +0200

    [#35] Simplify match_pos function

--- src/erlang_ls_poi.erl
+++ src/erlang_ls_poi.erl
@@ -3,6 +3,7 @@
 %%==============================================================================
 -module(erlang_ls_poi).
 
+%% Constructor
 -export([ new/4 ]).
 
 -export([ match_pos/2 ]).
@@ -19,7 +20,7 @@
 %% @edoc Constructor for a Point of Interest.
 -spec new(tree(), poi_kind(), any(), extra()) -> poi().
 new(Tree, Kind, Data, Extra) ->
-  Pos = erl_syntax:get_pos(Tree),
+  Pos   = erl_syntax:get_pos(Tree),
   Range = erlang_ls_range:range(Pos, Kind, Data, Extra),
   #{ kind  => Kind
    , data  => Data
@@ -28,12 +29,6 @@ new(Tree, Kind, Data, Extra) ->
 
 -spec match_pos([poi()], pos()) -> [poi()].
 match_pos(POIs, Pos) ->
-  [POI || #{range := Range} = POI <- POIs, matches_pos(Pos, Range)].
-
-%%==============================================================================
-%% Internal Functions
-%%==============================================================================
-
--spec matches_pos(pos(), poi_range()) -> boolean().
-matches_pos(Pos, #{from := From, to := To}) ->
-  (From =< Pos) andalso (Pos =< To).
+  [POI || #{range := #{ from := From
+                      , to    := To
+                      }} = POI <- POIs, (From =< Pos) andalso (Pos =< To)].
