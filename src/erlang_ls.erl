4ceb11a68f942955ed130376c388c347ade2d4a5
Juan Facorro <juan.facorro@klarna.com>
2019-10-29 09:47:14 +0100

    [#172] Escript with random node name

--- src/erlang_ls.erl
+++ src/erlang_ls.erl
@@ -4,6 +4,15 @@
 
 -spec main([any()]) -> ok.
 main([Port]) ->
+  init_node_name(),
   application:set_env(erlang_ls, port, list_to_integer(Port)),
   application:ensure_all_started(erlang_ls),
+  lager:info("Started erlang_ls server ~p", [node()]),
   receive _ -> ok end.
+
+-spec init_node_name() -> ok.
+init_node_name() ->
+  Name     = "erlang_ls_" ++ integer_to_list(rand:uniform(16#FFFFFFFFF)),
+  NodeName = list_to_atom(Name),
+  net_kernel:start([NodeName, shortnames]),
+  ok.
