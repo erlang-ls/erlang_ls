e46c5bcd832336677a7711483f07141d8174740e
Juan Facorro <juan.facorro@klarna.com>
2019-10-30 23:40:37 +0100

    [#184] Show only exported functions after colon

--- src/erlang_ls_text.erl
+++ src/erlang_ls_text.erl
@@ -6,6 +6,7 @@
 -export([ last_token/1
         , line/2
         , line/3
+        , tokens/1
         ]).
 
 -type text()       :: binary().
@@ -13,23 +14,28 @@
 -type column_num() :: non_neg_integer().
 -type token()      :: erl_scan:token().
 
-%% Extract the N-th line from a text.
+%% @doc Extract the N-th line from a text.
 -spec line(text(), line_num()) -> text().
 line(Text, LineNum) ->
   Lines = binary:split(Text, <<"\n">>, [global]),
   lists:nth(LineNum + 1, Lines).
 
-%% Extract the N-th line from a text, up to the given column number.
+%% @doc Extract the N-th line from a text, up to the given column number.
 -spec line(text(), line_num(), column_num()) -> text().
 line(Text, LineNum, ColumnNum) ->
   Line = line(Text, LineNum),
   binary:part(Line, {0, ColumnNum}).
 
-%% Extract the last token from the given text.
+%% @doc Return tokens from text.
+-spec tokens(text()) -> [any()].
+tokens(Text) ->
+  {ok, Tokens, _} = erl_scan:string(binary_to_list(Text)),
+  Tokens.
+
+%% @doc Extract the last token from the given text.
 -spec last_token(text()) -> token() | {error, empty}.
 last_token(Text) ->
-  {ok, Tokens, _} = erl_scan:string(binary_to_list(Text)),
-  case Tokens of
+  case tokens(Text) of
     [] -> {error, empty};
-    _  -> lists:last(Tokens)
+    Tokens -> lists:last(Tokens)
   end.
