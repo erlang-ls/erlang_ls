61bb3ad42104ae9868003d41d8162419e5f74574
Roberto Aloi <robertoaloi@users.noreply.github.com>
2019-12-21 21:30:34 +0100

    [#340] Update test suites

--- priv/code_navigation/src/diagnostics.new.erl
+++ priv/code_navigation/src/diagnostics.new.erl
@@ -2,6 +2,7 @@
 %% Changed diagnostics.erl, to test diff generation
 
 -include("diagnostics.hrl").
+-include("broken_diagnostics.hrl").
 
 -spec main(defined_type()) -> undefined_type().
 main(X) -> X + 1.
