704779accf333994f3f7c4456e4378b7f8ae138d
Roberto Aloi <robertoaloi@users.noreply.github.com>
2019-12-27 16:24:55 +0100

    [#363] Add implementation provider and support for gen_server callbacks

---
+++ src/els_implementation_provider.erl
@@ -0,0 +1,76 @@
+-module(els_implementation_provider).
+
+-behaviour(els_provider).
+
+-include("erlang_ls.hrl").
+
+-export([ handle_request/2
+        , is_enabled/0
+        ]).
+
+%%==============================================================================
+%% els_provider functions
+%%==============================================================================
+
+-spec is_enabled() -> boolean().
+is_enabled() ->
+  true.
+
+-spec handle_request(tuple(), els_provider:state()) ->
+   {null | location(), els_provider:state()}.
+handle_request({implementation, Params}, State) ->
+  #{ <<"position">>     := #{ <<"line">>      := Line
+                            , <<"character">> := Character
+                            }
+   , <<"textDocument">> := #{<<"uri">> := Uri}
+   } = Params,
+  {ok, Document} = els_utils:lookup_document(Uri),
+  case find_implementation(Document, Line, Character) of
+    [#{range := Range}|_] ->
+      {#{uri => Uri, range => els_protocol:range(Range)}, State};
+    [] ->
+      {null, State}
+  end.
+
+%%==============================================================================
+%% Internal functions
+%%==============================================================================
+-spec find_implementation( els_dt_document:item()
+                         , non_neg_integer()
+                         , non_neg_integer()
+                         ) -> [poi()].
+find_implementation(Document, Line, Character) ->
+  case els_dt_document:get_element_at_pos(Document, Line + 1, Character + 1) of
+    [POI|_] -> implementation(Document, POI);
+    []      -> []
+  end.
+
+-spec implementation(els_dt_document:item(), poi()) -> [poi()].
+implementation(Document, #{kind := application, id := MFA}) ->
+  case callback(MFA) of
+    {CF, CA} ->
+      POIs = els_dt_document:pois(Document, [function]),
+      [POI || #{id := {F, A}} = POI <- POIs, F =:= CF, A =:= CA];
+    undefined ->
+      []
+  end;
+implementation(_Document, _POI) ->
+  [].
+
+-spec callback(mfa()) -> {atom(), non_neg_integer()} | undefined.
+%% gen_server
+callback({gen_server, abcast, 2})     -> {handle_cast, 2};
+callback({gen_server, abcast, 3})     -> {handle_cast, 2};
+callback({gen_server, call, 2})       -> {handle_call, 3};
+callback({gen_server, call, 3})       -> {handle_call, 3};
+callback({gen_server, cast, 2})       -> {handle_cast, 2};
+callback({gen_server, multi_call, 2}) -> {handle_call, 3};
+callback({gen_server, multi_call, 3}) -> {handle_call, 3};
+callback({gen_server, multi_call, 4}) -> {handle_call, 3};
+callback({gen_server, start, 3})      -> {init, 1};
+callback({gen_server, start, 4})      -> {init, 1};
+callback({gen_server, start_link, 3}) -> {init, 1};
+callback({gen_server, start_link, 4}) -> {init, 1};
+callback({gen_server, stop, 1})       -> {terminate, 2};
+callback({gen_server, stop, 3})       -> {terminate, 2};
+callback(_)                           -> undefined.
---
+++ test/els_implementation_SUITE.erl
@@ -0,0 +1,76 @@
+-module(els_implementation_SUITE).
+
+-include("erlang_ls.hrl").
+
+%% CT Callbacks
+-export([ suite/0
+        , init_per_suite/1
+        , end_per_suite/1
+        , init_per_testcase/2
+        , end_per_testcase/2
+        , groups/0
+        , all/0
+        ]).
+
+%% Test cases
+-export([ gen_server_call/1
+        ]).
+
+%%==============================================================================
+%% Includes
+%%==============================================================================
+-include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+%%==============================================================================
+%% Types
+%%==============================================================================
+-type config() :: [{atom(), any()}].
+
+%%==============================================================================
+%% CT Callbacks
+%%==============================================================================
+-spec suite() -> [tuple()].
+suite() ->
+  [{timetrap, {seconds, 30}}].
+
+-spec all() -> [{group, stdio | tcp}].
+all() ->
+  [{group, tcp}, {group, stdio}].
+
+-spec groups() -> [atom()].
+groups() ->
+  els_test_utils:groups(?MODULE).
+
+-spec init_per_suite(config()) -> config().
+init_per_suite(Config) ->
+  els_test_utils:init_per_suite(Config).
+
+-spec end_per_suite(config()) -> ok.
+end_per_suite(Config) ->
+  els_test_utils:end_per_suite(Config).
+
+-spec init_per_testcase(atom(), config()) -> config().
+init_per_testcase(TestCase, Config) ->
+  els_test_utils:init_per_testcase(TestCase, Config).
+
+-spec end_per_testcase(atom(), config()) -> ok.
+end_per_testcase(TestCase, Config) ->
+  els_test_utils:end_per_testcase(TestCase, Config).
+
+%%==============================================================================
+%% Testcases
+%%==============================================================================
+
+-spec gen_server_call(config()) -> ok.
+gen_server_call(Config) ->
+  Uri = ?config(gen_server_uri, Config),
+  #{result := Result} = els_client:implementation(Uri, 30, 10),
+  Expected = #{ range =>
+                  #{ 'end' => #{character => 4,line => 46}
+                   , start => #{character => 0,line => 46}
+                   }
+              , uri => Uri
+              },
+  ?assertEqual(Expected, Result),
+  ok.
