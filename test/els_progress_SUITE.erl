8e99fb526ef25de082fbeb67ca306740bdfbf876
Roberto Aloi <robertoaloi@users.noreply.github.com>
2020-03-06 18:47:44 +0100

    [#285] Add stub for progress tests

---
+++ test/els_progress_SUITE.erl
@@ -0,0 +1,77 @@
+%%==============================================================================
+%% Tests for the 'progress' functionality
+%%==============================================================================
+-module(els_progress_SUITE).
+
+%%==============================================================================
+%% Common Test Callbacks
+%%==============================================================================
+-export([ suite/0
+        , init_per_suite/1
+        , end_per_suite/1
+        , init_per_testcase/2
+        , end_per_testcase/2
+        , groups/0
+        , all/0
+        ]).
+
+%%==============================================================================
+%% Testcases
+%%==============================================================================
+-export([ sample_job/1 ]).
+
+%%==============================================================================
+%% Includes
+%%==============================================================================
+-include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+-include("erlang_ls.hrl").
+
+%%==============================================================================
+%% Types
+%%==============================================================================
+-type config() :: [{atom(), any()}].
+
+%%==============================================================================
+%% CT Callbacks
+%%==============================================================================
+-spec suite() -> [tuple()].
+suite() ->
+  [{timetrap, {seconds, 30}}].
+
+-spec all() -> [{group, atom()}].
+all() ->
+  [{group, tcp}, {group, stdio}].
+
+-spec groups() -> [atom()].
+groups() ->
+  els_test_utils:groups(?MODULE).
+
+-spec init_per_suite(config()) -> config().
+init_per_suite(Config) ->
+  els_test_utils:init_per_suite(Config).
+
+-spec end_per_suite(config()) -> ok.
+end_per_suite(Config) ->
+  els_test_utils:end_per_suite(Config).
+
+-spec init_per_testcase(atom(), config()) -> config().
+init_per_testcase(TestCase, Config) ->
+  els_test_utils:init_per_testcase(TestCase, Config).
+
+-spec end_per_testcase(atom(), config()) -> ok.
+end_per_testcase(TestCase, Config) ->
+  els_test_utils:end_per_testcase(TestCase, Config).
+
+%%==============================================================================
+%% Testcases
+%%==============================================================================
+
+-spec sample_job(config()) -> ok.
+sample_job(_Config) ->
+  ok.
+
+%%==============================================================================
+%% Internal Functions
+%%==============================================================================
