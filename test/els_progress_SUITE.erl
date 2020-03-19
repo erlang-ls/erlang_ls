763313e4b39e06a6670d46e422a6d6da6d809b5d
Roberto Aloi <robertoaloi@users.noreply.github.com>
2020-03-19 09:18:46 +0100

    [#285] Final cleanup

--- test/els_progress_SUITE.erl
+++ test/els_progress_SUITE.erl
@@ -59,11 +59,18 @@ end_per_suite(Config) ->
   els_test_utils:end_per_suite(Config).
 
 -spec init_per_testcase(atom(), config()) -> config().
-init_per_testcase(TestCase, Config) ->
-  els_test_utils:init_per_testcase(TestCase, Config).
+init_per_testcase(sample_job = TestCase, Config) ->
+  Task = fun(_) -> ok end,
+  setup_mocks(Task),
+  [{task, Task} | els_test_utils:init_per_testcase(TestCase, Config)];
+init_per_testcase(failing_job = TestCase, Config) ->
+  Task = fun(_) -> throw(fail) end,
+  setup_mocks(Task),
+  [{task, Task} | els_test_utils:init_per_testcase(TestCase, Config)].
 
 -spec end_per_testcase(atom(), config()) -> ok.
 end_per_testcase(TestCase, Config) ->
+  teardown_mocks(),
   els_test_utils:end_per_testcase(TestCase, Config).
 
 %%==============================================================================
@@ -72,41 +79,16 @@ end_per_testcase(TestCase, Config) ->
 
 -spec sample_job(config()) -> ok.
 sample_job(_Config) ->
-  Entries = lists:seq(1, 27),
-  %% TODO: Move to init/end
-  meck:new(sample_job, [non_strict]),
-  meck:expect(sample_job, task, fun(_) -> ok end),
-  meck:expect(sample_job, on_complete, fun() -> ok end),
-  meck:expect(sample_job, on_error, fun() -> ok end),
-  Config = #{ task => fun sample_job:task/1
-            , entries => Entries
-            , on_complete => fun sample_job:on_complete/0
-            , on_error => fun sample_job:on_error/0
-            , title => <<"Sample job">>
-            },
-  %% TODO: Accept parameter with progress type ($/progress or showMessage)
-  {ok, Pid} = supervisor:start_child(els_background_job_sup, [Config]),
+  {ok, Pid} = new_background_job(),
   wait_for_completion(Pid),
-  ?assertEqual(length(Entries), meck:num_calls(sample_job, task, '_')),
+  ?assertEqual(length(entries()), meck:num_calls(sample_job, task, '_')),
   ?assertEqual(1, meck:num_calls(sample_job, on_complete, '_')),
   ?assertEqual(0, meck:num_calls(sample_job, on_error, '_')),
   ok.
 
 -spec failing_job(config()) -> ok.
 failing_job(_Config) ->
-  Entries = lists:seq(1, 27),
-  %% TODO: Move to init/end
-  meck:new(sample_job, [non_strict]),
-  meck:expect(sample_job, task, fun(_) -> throw(fail) end),
-  meck:expect(sample_job, on_complete, fun() -> ok end),
-  meck:expect(sample_job, on_error, fun() -> ok end),
-  Config = #{ task => fun sample_job:task/1
-            , entries => Entries
-            , on_complete => fun sample_job:on_complete/0
-            , on_error => fun sample_job:on_error/0
-            , title => <<"Sample job">>
-            },
-  {ok, Pid} = els_background_job:new(Config),
+  {ok, Pid} = new_background_job(),
   wait_for_completion(Pid),
   ?assertEqual(1, meck:num_calls(sample_job, task, '_')),
   ?assertEqual(0, meck:num_calls(sample_job, on_complete, '_')),
@@ -124,3 +106,30 @@ wait_for_completion(Pid) ->
     true ->
       timer:sleep(10)
   end.
+
+-spec setup_mocks(fun((_) -> ok)) -> ok.
+setup_mocks(Task) ->
+  meck:new(sample_job, [non_strict, no_link]),
+  meck:expect(sample_job, task, Task),
+  meck:expect(sample_job, on_complete, fun() -> ok end),
+  meck:expect(sample_job, on_error, fun() -> ok end),
+  ok.
+
+-spec teardown_mocks() -> ok.
+teardown_mocks() ->
+  meck:unload(sample_job),
+  ok.
+
+-spec new_background_job() -> {ok, pid()}.
+new_background_job() ->
+  Config = #{ task => fun sample_job:task/1
+            , entries => entries()
+            , on_complete => fun sample_job:on_complete/0
+            , on_error => fun sample_job:on_error/0
+            , title => <<"Sample job">>
+            },
+  els_background_job:new(Config).
+
+-spec entries() -> [any()].
+entries() ->
+  lists:seq(1, 27).
