8f5cb703bd5abb251045ef13279fce89767c086a
Roberto Aloi <robertoaloi@users.noreply.github.com>
2020-03-25 09:29:38 +0100

    [#606] Do not crash if pretty-printing fails

--- src/els_parser.erl
+++ src/els_parser.erl
@@ -88,7 +88,7 @@ find_attribute_pois(Tree, Tokens) ->
         {spec, {spec, {{F, A}, FTs}}} ->
           From = erl_syntax:get_pos(Tree),
           To   = erl_scan:location(lists:last(Tokens)),
-          Data = els_utils:to_binary(erl_prettypr:format(Tree)),
+          Data = pretty_print_spec(Tree),
           [ poi({From, To}, spec, {F, A}, Data)
           | lists:flatten([find_spec_points_of_interest(FT) || FT <- FTs])
           ];
@@ -472,3 +472,12 @@ subtrees(Tree, attribute) ->
   end;
 subtrees(Tree, _) ->
   erl_syntax:subtrees(Tree).
+
+-spec pretty_print_spec(tree()) -> binary().
+pretty_print_spec(Tree) ->
+  try
+    els_utils:to_binary(erl_prettypr:format(Tree))
+ catch
+   _:_:_ ->
+     <<>>
+ end.
