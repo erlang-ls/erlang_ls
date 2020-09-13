5d7bbf6ad5b970772b6dac7c6662347900734274
Juan Facorro <juan.facorro@klarna.com>
2020-09-13 17:21:40 +0200

    [SF] Remove unused els_config children from supervisor

--- src/els_dap_sup.erl
+++ src/els_dap_sup.erl
@@ -44,11 +44,7 @@ init([]) ->
   lager:info("Starting session (version ~p)", [Vsn]),
   %% Restrict access to stdio when using that transport
   restrict_stdio_access(Transport),
-  ChildSpecs = [ #{ id       => els_config
-                  , start    => {els_config, start_link, []}
-                  , shutdown => brutal_kill
-                  }
-               , #{ id    => els_dap_providers_sup
+  ChildSpecs = [ #{ id    => els_dap_providers_sup
                   , start => {els_dap_providers_sup, start_link, []}
                   , type  => supervisor
                   }
