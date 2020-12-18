ae8e8468664296ef4c23ecf27c4a8fb5f6225d48
Lukas Larsson <lukas@erlang.org>
2020-12-18 18:13:13 +0100

    Add highlight tests for type, export_type_entr, import_entry

--- test/els_document_highlight_SUITE.erl
+++ test/els_document_highlight_SUITE.erl
@@ -18,6 +18,9 @@
         , fun_local/1
         , fun_remote/1
         , export_entry/1
+        , export_type_entry/1
+        , import_entry/1
+        , type/1
         , macro/1
         ]).
 
@@ -124,6 +127,39 @@ export_entry(Config) ->
   Uri = ?config(code_navigation_uri, Config),
   #{result := Locations} = els_client:document_highlight(Uri, 5, 25),
   ExpectedLocations = expected_definitions(),
+  assert_locations(ExpectedLocations, Locations),
+  ok.
+
+-spec export_type_entry(config()) -> ok.
+export_type_entry(Config) ->
+  Uri = ?config(code_navigation_types_uri, Config),
+  #{result := Locations} = els_client:document_highlight(Uri, 5, 16),
+  ExpectedLocations = [ #{range => #{from => {5, 16}, to => {5, 24}}}
+                        %% Should also include the definition, but does not
+                        %%, #{range => #{from => {3, 7}, to => {3, 13}}}
+                      ],
+  assert_locations(ExpectedLocations, Locations),
+  ok.
+
+-spec import_entry(config()) -> ok.
+import_entry(Config) ->
+  Uri = ?config(code_navigation_uri, Config),
+  #{result := Locations} = els_client:document_highlight(Uri, 10, 34),
+  ExpectedLocations = [ #{range => #{from => {10, 34}, to => {10, 38}}}
+                        %% Should include uses of function but does not
+                        %% , #{range => #{from => {90, 3}, to => {90, 5}}}
+                      ],
+  assert_locations(ExpectedLocations, Locations),
+  ok.
+
+-spec type(config()) -> ok.
+type(Config) ->
+  Uri = ?config(code_navigation_uri, Config),
+  #{result := Locations} = els_client:document_highlight(Uri, 37, 9),
+  ExpectedLocations = [ #{range => #{from => {37, 7}, to => {37, 13}}}
+                        %% Should also include the usage, but does not
+                        %%, #{range => #{from => {55, 23}, to => {55, 29}}}
+                      ],
   assert_locations(ExpectedLocations, Locations),
   ok.
 
