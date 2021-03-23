8edbd6c7f31b594b02b9b9c656e5802e9fb0859d
Lukas Larsson <lukas@erlang.org>
2021-03-23 15:27:17 +0100

    Refactor and try again

--- apps/els_lsp/src/els_gradualizer_diagnostics.erl
+++ apps/els_lsp/src/els_gradualizer_diagnostics.erl
@@ -38,37 +38,14 @@ is_default() ->
 
 -spec run(uri()) -> [els_diagnostics:diagnostic()].
 run(Uri) ->
-
-    try application:ensure_all_started(gradualizer) of
-        {ok, _} ->
+    case start_and_load() of
+        true ->
             Path = unicode:characters_to_list(els_uri:path(Uri)),
-            load_dependencies(),
             Includes = [{i, I} || I <- els_config:get(include_paths)],
             Opts = [return_errors] ++ Includes,
             Errors = gradualizer:type_check_files([Path], Opts),
-            lists:flatmap(
-              fun({ErrorPath, Error}) when Path =:= ErrorPath ->
-                      FmtError = gradualizer_fmt:format_type_error(
-                                   Error
-                                  , [{fmt_location, brief}, {color, never}]),
-                      case re:run(
-                             FmtError, "([0-9]+):([0-9]+:)? (.*)",
-                             [{capture, all_but_first, binary}]) of
-                          {match, [BinLine, _BinCol, Msg]} ->
-                              Line = binary_to_integer(BinLine),
-                              Range = els_protocol:range(
-                                        #{ from => {Line, 1},
-                                           to => {Line + 1, 1} }),
-                              [#{ range => Range,
-                                 message => Msg,
-                                 severity => ?DIAGNOSTIC_WARNING,
-                                 source => source() }];
-                          _ ->
-                              []
-                      end
-              end, Errors)
-    catch E:R:ST ->
-            ?LOG_ERROR("Errors: ~p", [{E, R, ST}]),
+            lists:flatmap(fun analyzer_error/1, Errors);
+        false ->
             []
     end.
 
@@ -80,12 +57,47 @@ source() ->
 %% Internal Functions
 %%==============================================================================
 
--spec load_dependencies() -> ok.
-load_dependencies() ->
-    Files = lists:flatmap(
-              fun(Dir) -> filelib:wildcard(filename:join(Dir, "*.erl")) end,
-              els_config:get(apps_paths) ++ els_config:get(deps_paths)),
-    ok = gradualizer_db:import_erl_files(Files).
+-spec start_and_load() -> boolean().
+start_and_load() ->
+    try application:ensure_all_started(gradualizer) of
+        {ok, [gradualizer]} ->
+            Files = lists:flatmap(
+                      fun(Dir) ->
+                              filelib:wildcard(filename:join(Dir, "*.erl"))
+                      end,
+                      els_config:get(apps_paths) ++ els_config:get(deps_paths)),
+            ok = gradualizer_db:import_erl_files(Files),
+            true;
+        _ ->
+            false
+    catch E:R ->
+            ?LOG_ERROR("Could not start gradualizer: ~p ~p", [E, R]),
+            false
+    end.
+
+-spec analyzer_error(any()) -> any().
+analyzer_error({_Path, Error}) ->
+    FmtError = gradualizer_fmt:format_type_error(
+                 Error
+                , [{fmt_location, brief}, {color, never}]),
+    case re:run(
+           FmtError, "([0-9]+):([0-9]+:)? (.*)",
+           [{capture, all_but_first, binary}]) of
+        {match, [BinLine, _BinCol, Msg]} ->
+            Line = case binary_to_integer(BinLine) of
+                       0 -> 1;
+                       L -> L
+                   end,
+            Range = els_protocol:range(
+                      #{ from => {Line, 1},
+                         to => {Line + 1, 1} }),
+            [#{ range => Range,
+                message => Msg,
+                severity => ?DIAGNOSTIC_WARNING,
+                source => source() }];
+        _ ->
+            []
+    end.
 
 %% -spec dep_path(module()) -> string().
 %% dep_path(Module) ->
