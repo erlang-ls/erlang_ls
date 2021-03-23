71de2df52ebd63e3cc9e214afad8e44f6674272c
Lukas Larsson <lukas@erlang.org>
2021-03-23 11:56:42 +0100

    Add better error reporting

--- apps/els_lsp/src/els_gradualizer_diagnostics.erl
+++ apps/els_lsp/src/els_gradualizer_diagnostics.erl
@@ -39,30 +39,40 @@ is_default() ->
 -spec run(uri()) -> [els_diagnostics:diagnostic()].
 run(Uri) ->
     Path = els_uri:path(Uri),
-    ?LOG_ERROR("Path: ~p",[Path]),
+    {ok, Document} = els_utils:lookup_document(Uri),
     try gradualizer:type_check_file(
           unicode:characters_to_list(Path),
           [return_errors]) of
         Errors ->
-            ?LOG_ERROR("Errors: ~p",[Errors]),
-            FmtErrors = [gradualizer_fmt:format_type_error(Error,[{fmt_location,brief}])
-             || {_Path,Error} <- Errors],
-            ?LOG_ERROR("FmtErrors: ~p",[FmtErrors]),
-            lists:flatten(
-              [ case re:run(
-                     Str, "([0-9]+):([0-9]+)(.*)",
-                     [{capture,all_but_first,binary}]) of
-                  {match, [Line,Col,Msg]} ->
-                      #{ range => els_protocol:range(
-                                    #{ from => {binary_to_integer(Line),
-                                                binary_to_integer(Col)},
-                                       to => {binary_to_integer(Line) + 1, 1} }),
-                         message => Msg,
-                         severity => ?DIAGNOSTIC_WARNING,
-                         source => source() };
-                  _ ->
-                      []
-              end || Str <- FmtErrors])
+            lists:flatmap(
+              fun({_Path,Error}) ->
+                      FmtError = gradualizer_fmt:format_type_error(
+                                   Error
+                                  , [{fmt_location, brief}, {color, never}]),
+                      case re:run(
+                             FmtError, "([0-9]+):([0-9]+)(.*)",
+                             [{capture, all_but_first, binary}]) of
+                          {match, [BinLine, BinCol, Msg]} ->
+                              Line = binary_to_integer(BinLine),
+                              Col = binary_to_integer(BinCol),
+                              ?LOG_ERROR("Get element at pos: ~p",[{Line,Col}]),
+                              Range = case
+                                          els_dt_document:get_element_at_pos(Document, Line, Col)
+                                      of
+                                          [#{ range := R } | _] -> R;
+                                          []        ->
+                                              els_protocol:range(
+                                                #{ from => {Line,1},
+                                                   to => {Line + 1, 1} })
+                                      end,
+                              [#{ range => Range,
+                                 message => Msg,
+                                 severity => ?DIAGNOSTIC_WARNING,
+                                 source => source() }];
+                          _ ->
+                              []
+                      end
+              end, Errors)
     catch E:R:ST ->
             ?LOG_ERROR("Errors: ~p",[{E,R,ST}]),
             []
