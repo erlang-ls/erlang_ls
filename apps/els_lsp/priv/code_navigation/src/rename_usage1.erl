581ac0e5bd910c8bad483f9c1e9a085b7e6d4851
Péter Gömöri <gomoripeti@gmail.com>
2021-05-24 12:37:31 +0200

    Support renaming records and record fields

--- apps/els_lsp/priv/code_navigation/src/rename_usage1.erl
+++ apps/els_lsp/priv/code_navigation/src/rename_usage1.erl
@@ -17,3 +17,11 @@ rename_me_macro() ->
 
 rename_me_parametrized_macro() ->
   {?RENAME_ME_PARAMETRIZED(1), ?RENAME_ME_PARAMETRIZED(2)}.
+
+rename_record(R = #rename_rec{rename_field = F}) ->
+  %% field access
+  F = R#rename_rec.rename_field,
+  %% record create
+  R2 = #rename_rec{rename_field = 12},
+  %% record update
+  R2#rename_rec{rename_field = F}.
