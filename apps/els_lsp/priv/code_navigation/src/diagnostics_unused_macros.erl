adbcbc9380d032f0ed70ad79e688716e6366a8b5
Péter Gömöri <gomoripeti@gmail.com>
2021-06-09 23:24:53 +0200

    Fix creating macro POIs in function applications

--- apps/els_lsp/priv/code_navigation/src/diagnostics_unused_macros.erl
+++ apps/els_lsp/priv/code_navigation/src/diagnostics_unused_macros.erl
@@ -4,6 +4,8 @@
 
 -define(USED_MACRO, used_macro).
 -define(UNUSED_MACRO, unused_macro).
+-define(MOD, module). %% MOD was incorrectly reported as unused (#1021)
 
 main() ->
+  ?MOD:foo(),
   ?USED_MACRO.
