cf8accee5f26985f80ef200bd0b1cd23fa5d87a5
Péter Gömöri <gomoripeti@gmail.com>
2021-10-03 23:19:52 +0200

    Move macro related parser tests to separate suite

--- apps/els_lsp/test/els_parser_SUITE.erl
+++ apps/els_lsp/test/els_parser_SUITE.erl
@@ -20,14 +20,6 @@
         , types_recursive/1
         , opaque_recursive/1
         , record_def_recursive/1
-        , callback_macro/1
-        , spec_macro/1
-        , type_macro/1
-        , opaque_macro/1
-        , wild_attrbibute_macro/1
-        , type_name_macro/1
-        , spec_name_macro/1
-        , macro_in_application/1
         , var_in_application/1
         , unicode_clause_pattern/1
         , latin1_source_code/1
@@ -36,7 +28,6 @@
 %%==============================================================================
 %% Includes
 %%==============================================================================
--include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
 -include("els_lsp.hrl").
 
@@ -183,85 +174,6 @@ assert_recursive_types(Text) ->
                 #{id := {t2, 1}},
                 #{id := {t3, 0}}],
                parse_find_pois(Text, type_application)),
-  ok.
-
--spec callback_macro(config()) -> ok.
-callback_macro(_Config) ->
-  Text = "-callback foo() -> ?M.",
-  ?assertMatch([_], parse_find_pois(Text, callback, {foo, 0})),
-  ?assertMatch([_], parse_find_pois(Text, macro, 'M')),
-  ok.
-
--spec spec_macro(config()) -> ok.
-spec_macro(_Config) ->
-  Text = "-spec foo() -> ?M().",
-  ?assertMatch([_], parse_find_pois(Text, spec, {foo, 0})),
-  ?assertMatch([_], parse_find_pois(Text, macro, {'M', 0})),
-  ok.
-
--spec type_macro(config()) -> ok.
-type_macro(_Config) ->
-  Text = "-type t() :: ?M(a, b, c).",
-  ?assertMatch([_], parse_find_pois(Text, type_definition, {t, 0})),
-  ?assertMatch([_], parse_find_pois(Text, macro, {'M', 3})),
-  ok.
-
--spec opaque_macro(config()) -> ok.
-opaque_macro(_Config) ->
-  Text = "-opaque o() :: ?M(a, b).",
-  ?assertMatch([_], parse_find_pois(Text, type_definition, {o, 0})),
-  ?assertMatch([_], parse_find_pois(Text, macro, {'M', 2})),
-  ok.
-
--spec wild_attrbibute_macro(config()) -> ok.
-wild_attrbibute_macro(_Config) ->
-  %% This is parsed as -(?M(foo)), rather than -(?M)(foo)
-  Text = "-?M(foo).",
-  ?assertMatch([_], parse_find_pois(Text, macro, {'M', 1})),
-  ?assertMatch([_], parse_find_pois(Text, atom, foo)),
-  ok.
-
-type_name_macro(_Config) ->
-  %% Currently els_dodger cannot parse this type definition
-  %% Verify this does not prevent parsing following forms
-  Text = "-type ?M() -> integer() | t(). -spec f() -> any().",
-  ?assertMatch({ok, [#{kind := spec, id := {f, 0}}]}, els_parser:parse(Text)),
-  ok.
-
-spec_name_macro(_Config) ->
-  %% Verify the parser does not crash on macros in spec function names and it
-  %% still returns an unnamed spec-context and POIs from the definition body
-  Text1 = "-spec ?M() -> integer() | t().",
-  ?assertMatch([#{id := undefined}], parse_find_pois(Text1, spec)),
-  ?assertMatch([_], parse_find_pois(Text1, type_application, {t, 0})),
-
-  Text2 = "-spec ?MODULE:b() -> integer() | t().",
-  ?assertMatch([#{id := undefined}], parse_find_pois(Text2, spec)),
-  %% TODO: Update erlfmt, a later version can parse this
-  %%?assertMatch([_], parse_find_pois(Text2, type_application, {t, 0})),
-  ok.
-
-macro_in_application(_Config) ->
-  Text1 = "f() -> ?M:f(42).",
-  ?assertMatch([#{id := 'M'}], parse_find_pois(Text1, macro)),
-
-  Text2 = "f() -> ?M(mod):f(42).",
-  ?assertMatch([#{id := {'M', 1}}], parse_find_pois(Text2, macro)),
-
-  %% This is not an application, only a module qualifier before macro M/1
-  Text3 = "f() -> mod:?M(42).",
-  ?assertMatch([#{id := {'M', 1}}], parse_find_pois(Text3, macro)),
-
-  %% Application with macro M/0 as function name
-  Text4 = "f() -> mod:?M()(42).",
-  ?assertMatch([#{id := {'M', 0}}], parse_find_pois(Text4, macro)),
-
-  %% Known limitation of the current implementation,
-  %% ?MODULE is handled specially, converted to a local call
-  Text5 = "f() -> ?MODULE:foo().",
-  ?assertMatch([], parse_find_pois(Text5, macro)),
-  ?assertMatch([#{id := {foo, 0}}], parse_find_pois(Text5, application)),
-
   ok.
 
 var_in_application(_Config) ->
