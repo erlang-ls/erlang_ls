5c721ce409aa1f33cd089c079c74bf159685f83e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-16 09:58:43 +0200

    Format els_gradualizer_diagnostics according to .editorconfig

--- apps/els_lsp/src/els_gradualizer_diagnostics.erl
+++ apps/els_lsp/src/els_gradualizer_diagnostics.erl
@@ -38,16 +38,16 @@ is_default() ->
 
 -spec run(uri()) -> [els_diagnostics:diagnostic()].
 run(Uri) ->
-    case start_and_load() of
-        true ->
-            Path = unicode:characters_to_list(els_uri:path(Uri)),
-            Includes = [{i, I} || I <- els_config:get(include_paths)],
-            Opts = [return_errors] ++ Includes,
-            Errors = gradualizer:type_check_files([Path], Opts),
-            lists:flatmap(fun analyzer_error/1, Errors);
-        false ->
-            []
-    end.
+  case start_and_load() of
+    true ->
+      Path = unicode:characters_to_list(els_uri:path(Uri)),
+      Includes = [{i, I} || I <- els_config:get(include_paths)],
+      Opts = [return_errors] ++ Includes,
+      Errors = gradualizer:type_check_files([Path], Opts),
+      lists:flatmap(fun analyzer_error/1, Errors);
+    false ->
+      []
+  end.
 
 -spec source() -> binary().
 source() ->
@@ -59,49 +59,48 @@ source() ->
 
 -spec start_and_load() -> boolean().
 start_and_load() ->
-    try application:ensure_all_started(gradualizer) of
-        {ok, [gradualizer]} ->
-            Files = lists:flatmap(
-                      fun(Dir) ->
-                              filelib:wildcard(filename:join(Dir, "*.erl"))
-                      end,
-                      els_config:get(apps_paths) ++ els_config:get(deps_paths)),
-            case erlang:function_exported(
-                   gradualizer_db, import_erl_files, 2) of
-                true ->
-                    ok = gradualizer_db:import_erl_files(
-                           Files,
-                           els_config:get(include_paths));
-                false ->
-                    ok = gradualizer_db:import_erl_files(Files)
-            end,
-            true;
-        {ok, []} ->
-            true
-    catch E:R ->
-            ?LOG_ERROR("Could not start gradualizer: ~p ~p", [E, R]),
-            false
-    end.
+  try application:ensure_all_started(gradualizer) of
+    {ok, [gradualizer]} ->
+      Files = lists:flatmap(
+                fun(Dir) ->
+                    filelib:wildcard(filename:join(Dir, "*.erl"))
+                end,
+                els_config:get(apps_paths) ++ els_config:get(deps_paths)),
+      case erlang:function_exported(
+             gradualizer_db, import_erl_files, 2) of
+        true ->
+          ok = gradualizer_db:import_erl_files(
+                 Files,
+                 els_config:get(include_paths));
+        false ->
+          ok = gradualizer_db:import_erl_files(Files)
+      end,
+      true;
+    {ok, []} ->
+      true
+  catch E:R ->
+          ?LOG_ERROR("Could not start gradualizer: ~p ~p", [E, R]),
+          false
+  end.
 
 -spec analyzer_error(any()) -> any().
 analyzer_error({_Path, Error}) ->
-    FmtOpts = [{fmt_location, brief}, {color, never}],
-    FmtError = gradualizer_fmt:format_type_error(Error, FmtOpts),
-    case re:run(
-           FmtError, "([0-9]+):([0-9]+:)? (.*)",
-           [{capture, all_but_first, binary}]) of
-        {match, [BinLine, _BinCol, Msg]} ->
-            Line = case binary_to_integer(BinLine) of
-                       0 -> 1;
-                       L -> L
-                   end,
-            Range = els_protocol:range(
-                      #{ from => {Line, 1},
-                         to => {Line + 1, 1} }),
-            [#{ range => Range,
-                message => Msg,
-                severity => ?DIAGNOSTIC_WARNING,
-                source => source() }];
-        _ ->
-            []
-    end.
+  FmtOpts = [{fmt_location, brief}, {color, never}],
+  FmtError = gradualizer_fmt:format_type_error(Error, FmtOpts),
+  case re:run(FmtError, "([0-9]+):([0-9]+:)? (.*)",
+              [{capture, all_but_first, binary}]) of
+    {match, [BinLine, _BinCol, Msg]} ->
+      Line = case binary_to_integer(BinLine) of
+               0 -> 1;
+               L -> L
+             end,
+      Range = els_protocol:range(
+                #{ from => {Line, 1},
+                   to => {Line + 1, 1} }),
+      [#{ range => Range,
+          message => Msg,
+          severity => ?DIAGNOSTIC_WARNING,
+          source => source() }];
+    _ ->
+      []
+  end.
