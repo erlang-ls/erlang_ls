222ac7505e318e44b9398a49d972d1ffd687ad6c
Robert Fiko <fiko.robert@gmail.com>
2021-11-26 18:42:43 +0100

    Changed to map, to extend confiureability

--- apps/els_core/src/els_config.erl
+++ apps/els_core/src/els_config.erl
@@ -77,7 +77,7 @@
                   , indexing_enabled => boolean()
                   , bsp_enabled      => boolean() | auto
                   , compiler_telemetry_enabled => boolean()
-                  , refactorerl => {atom(), atom() | 'disabled'}
+                  , refactorerl => { map() | 'disabled'}
                   }.
 
 %%==============================================================================
@@ -128,7 +128,7 @@ do_initialize(RootUri, Capabilities, InitOptions, {ConfigPath, Config}) ->
 
   IndexingEnabled = maps:get(<<"indexingEnabled">>, InitOptions, true),
 
-  RefactorErl = {config, maps:get("refactorerl", Config, disabled)},
+  RefactorErl = maps:get("refactorerl", Config, disabled),
 
   %% Passed by the LSP client
   ok = set(root_uri       , RootUri),
