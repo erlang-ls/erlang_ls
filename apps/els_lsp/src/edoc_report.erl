f1b9f7b4f9ea4594af3165721ecbf4509c225343
Roberto Aloi <robertoaloi@fb.com>
2022-02-18 13:43:13 +0100

    Fix include, indent file

--- apps/els_lsp/src/edoc_report.erl
+++ apps/els_lsp/src/edoc_report.erl
@@ -33,69 +33,69 @@
 %% Avoid warning for local functions error/{1,2,3} clashing with autoimported BIF.
 -compile({no_auto_import, [error/1, error/2, error/3]}).
 -export([error/1,
-	 error/2,
-	 error/3,
-	 report/2,
-	 report/3,
-	 report/4,
-	 warning/1,
-	 warning/2,
-	 warning/3,
-	 warning/4]).
+         error/2,
+         error/3,
+         report/2,
+         report/3,
+         report/4,
+         warning/1,
+         warning/2,
+         warning/3,
+         warning/4]).
 
--include("edoc.hrl").
+-include_lib("edoc/src/edoc.hrl").
 
 
 error(What) ->
-    error([], What).
+  error([], What).
 
 error(Where, What) ->
-    error(0, Where, What).
+  error(0, Where, What).
 
 error(Line, Where, S) when is_list(S) ->
-    report(Line, Where, S, []);
+  report(Line, Where, S, []);
 error(Line, Where, {S, D}) when is_list(S) ->
-    report(Line, Where, S, D);
+  report(Line, Where, S, D);
 error(Line, Where, {format_error, M, D}) ->
-    report(Line, Where, M:format_error(D), []).
+  report(Line, Where, M:format_error(D), []).
 
 warning(S) ->
-    warning(S, []).
+  warning(S, []).
 
 warning(S, Vs) ->
-    warning([], S, Vs).
+  warning([], S, Vs).
 
 warning(Where, S, Vs) ->
-    warning(0, Where, S, Vs).
+  warning(0, Where, S, Vs).
 
 warning(L, Where, S, Vs) ->
-    report(L, Where, "warning: " ++ S, Vs).
+  report(L, Where, "warning: " ++ S, Vs).
 
 report(S, Vs) ->
-    report([], S, Vs).
+  report([], S, Vs).
 
 report(Where, S, Vs) ->
-    report(0, Where, S, Vs).
+  report(0, Where, S, Vs).
 
 report(L, Where, S, Vs) ->
-    io:put_chars(where(Where)),
-    if is_integer(L), L > 0 ->
+  io:put_chars(where(Where)),
+  if is_integer(L), L > 0 ->
 	    io:fwrite("at line ~w: ", [L]);
-       true ->
+     true ->
 	    ok
-    end,
-    io:fwrite(S, Vs),
-    io:nl().
+  end,
+  io:fwrite(S, Vs),
+  io:nl().
 
 where({File, module}) ->
-    io_lib:fwrite("~ts, in module header: ", [File]);
+  io_lib:fwrite("~ts, in module header: ", [File]);
 where({File, footer}) ->
-    io_lib:fwrite("~ts, in module footer: ", [File]);
+  io_lib:fwrite("~ts, in module footer: ", [File]);
 where({File, header}) ->
-    io_lib:fwrite("~ts, in header file: ", [File]);
+  io_lib:fwrite("~ts, in header file: ", [File]);
 where({File, {F, A}}) ->
-    io_lib:fwrite("~ts, function ~ts/~w: ", [File, F, A]);
+  io_lib:fwrite("~ts, function ~ts/~w: ", [File, F, A]);
 where([]) ->
-    io_lib:fwrite("~s: ", [?APPLICATION]);
+  io_lib:fwrite("~s: ", [?APPLICATION]);
 where(File) when is_list(File) ->
-    File ++ ": ".
+  File ++ ": ".
