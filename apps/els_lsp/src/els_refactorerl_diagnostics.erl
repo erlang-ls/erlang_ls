1389a3089096173e992e524f83be0183f6227210
Robert Fiko <64466886+robertfiko@users.noreply.github.com>
2022-03-13 11:07:17 +0100

    Diagnostic config option

--- apps/els_lsp/src/els_refactorerl_diagnostics.erl
+++ apps/els_lsp/src/els_refactorerl_diagnostics.erl
@@ -66,32 +66,31 @@ source() ->
 %% Internal Functions
 %%==============================================================================
 
-% ddoocc
+% @doc
 % Returns the enabled diagnostic aliases from config
-%-spec configured_diagnostics() -> sets:set().
-%configured_diagnostics() ->
-%  case els_config:get(refactorerl) of
-%    #{"diagnostics" := List} ->
-%      sets:from_list(List);
-%    _ ->
-%      []
-%  end.
+-spec configured_diagnostics() -> sets:set().
+configured_diagnostics() ->
+  case els_config:get(refactorerl) of
+    #{"diagnostics" := List} ->
+      AtomList = [list_to_atom(Element) || Element <- List],
+      sets:from_list(AtomList);
+    _ ->
+      []
+  end.
 
-% ddoocc
+% @doc
 % Returns the default diagnostic aliases
-%-spec default_diagnostics() -> sets:set().
-%default_diagnostics() ->
-%  sets:from_list(["unused_macros", "unsecure_os_call"]).
+-spec default_diagnostics() -> sets:set().
+default_diagnostics() ->
+  sets:from_list([unused_macros, unsecure_os_call]).
 
 
 % @doc
 % Returns the enabled diagnostics by merging default and configed
 -spec enabled_diagnostics() -> [refactorerl_diagnostic_alias()].
 enabled_diagnostics() ->
-  %Set = sets:union(default_diagnostics(), configured_diagnostics()),
-  %sets:to_list(Set), %TODO Set operation
-  %["unused_macros", "unsecure_os_call"].
-  [unused_macros, unsecure_os_call].
+  Set = sets:union(default_diagnostics(), configured_diagnostics()),
+  sets:to_list(Set).
 
 
 % @doc
