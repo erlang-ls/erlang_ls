9bac30f5dcd6b9c23d2c7879b8b93cac82d73600
Roberto Aloi <robertoaloi@fb.com>
2022-06-16 19:04:16 +0200

    Single access

--- apps/els_lsp/src/els_erlang_provider.erl
+++ apps/els_lsp/src/els_erlang_provider.erl
@@ -20,18 +20,32 @@ handle_request({edoc, Params}) ->
     #{
       <<"textDocument">> := #{<<"uri">> := Uri}
      } = Params,
-    M = els_uri:module(Uri),
-    {ok, Doc} = els_utils:lookup_document(Uri),
-    POIs = els_dt_document:pois(Doc, [function]),
-    %% TODO: Add module docs
-    %% TODO: Only compute once
+    %% M = els_uri:module(Uri),
+    %% {ok, Doc} = els_utils:lookup_document(Uri),
+    %% POIs = els_dt_document:pois(Doc, [function]),
     %% TODO: Background job
-    Output = [begin
-                  #{value := Value} = els_markup_content:new(els_docs:edoc(M, F, A)),
-                  Value
-              end || #{id := {F, A}} <- POIs],
-    {response, #{name => unicode:characters_to_binary(lists:join("\n\n", Output))}}.
+    %% TODO: Markdown?
+    %% Output = [begin
+    %%               #{value := Value} = els_markup_content:new(els_docs:edoc(M, F, A)),
+    %%               Value
+    %%           end || #{id := {F, A}} <- POIs],
+    %% {response, #{name => unicode:characters_to_binary(lists:join("\n\n", Output))}}.
+    {response, #{name => get_docs(Uri)}}.
 
 %%==============================================================================
 %% Internal Functions
 %%==============================================================================
+-spec get_docs(uri()) -> binary().
+get_docs(Uri) ->
+    Module = els_uri:module(Uri),
+    case els_docs:get_edoc_chunk(Module, Uri) of
+        error ->
+            <<"Error while producing EDoc">>;
+        {ok, Chunk} ->
+            ModuleDocs = shell_docs:render(Module, Chunk),
+            %% TODO: Is there a single API?
+            {ok, [Document]} = els_dt_document:lookup(Uri),
+            Functions = els_dt_document:pois(Document, [function]),
+            FunctionsDocs = [shell_docs:render(Module, F, A, Chunk) || #{id := {F, A}} <- Functions],
+            unicode:characters_to_binary([ModuleDocs | FunctionsDocs])
+    end.
